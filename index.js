68a468d7c6e0f6308f601701f83227d4b49595c2
Pablo <pabloagronomo@gmail.com>
2019-06-17 13:23:19 -0400

    jwt integration

--- index.js
+++ index.js
@@ -1,75 +1,127 @@
 const si = require('systeminformation');
-var express = require('express');
-var app = express();
-const sqlite3 = require('sqlite3');
-const dbpath = "./mydb.sqlite3";
+const express = require('express');
+const { readFileSync } = require('fs');
+const { Database } = require('sqlite3');
+const { sign, verify } = require('jsonwebtoken');
+
+const privateKEY = readFileSync('./private.key', 'utf8');
+const publicKEY = readFileSync('./public.key', 'utf8');
+const app = express();
+
+// SIGNING OPTIONS
+const signOptions = {
+    issuer: "SPEC",
+    subject: "pcid@spec.cl",
+    audience: "http://spec.cl/",
+    expiresIn: "30m",
+    algorithm: "RS256"
+};
+
+/*
+ ====================   JWT Verify =====================
+*/
+const verifyOptions = {
+    issuer: "SPEC",
+    subject: "pcid@spec.cl",
+    audience: "http://spec.cl/",
+    expiresIn: "30m",
+    algorithm: ["RS256"]
+};
+
+
+// setting the enviromental variables
+const SQLITE_PATH_FILE = process.env.SQLITE_PATH_FILE;
+const PORT = process.env.PORT;
+const APIKEY = process.env.APIKEY;
+const MAX_RECORDS = process.env.MEMORY_RECORDS || 100;
+const SECONDS_INTERVAL = process.env.SECONDS_INTERVAL || 60;
+let defaultMemoryRecords = 10;
+
+// api hashmap
 const apiKeys = new Map();
-let defaultMemoryRecords = 100;
-
-apiKeys.set('jbspbNJd3FICaiyMR8VckzylDdiONllc', {
-    id: 1,
-    name: 'Pablo',
-});
-
-apiKeys.set('nPL2k5JaOOjvZkmaBbW5ZF5qZBTrKdOJ', {
-    id: 2,
-    name: 'Fernando'
-});
+apiKeys.set(APIKEY, { id: 1, name: 'Api key USER' });
 
 // middleware for checking the apikey
-app.use((req, res, next) => {
-    console.log(req.query);
-    if (!apiKeys.has(req.query.apikey)) {
-        res.status(401).send('Unknown api key');
-        return;
-    } {
-        next();
-    }
-});
+const apiKeyHandler = (req, res, next) => {
+    // if (!req.query.apikey || !req.query.token) { res.status(401).send('api key or token do not exist!'); return; }
 
-let db = new sqlite3.Database(dbpath, (err) => {
+    if (apiKeys.has(req.query.apikey)) {
+        req.authType = "apikey";
+        next();
+    } else {
+        try {
+            req.jwtPayload = verify(req.query.token, publicKEY, verifyOptions);
+            req.authType = "token";
+            next();
+        }
+        catch (error) {
+            res.status(401).send('Forbidden');
+        }
+    }
+
+}
+app.use(apiKeyHandler);
+
+// setting the sqlite and run the service
+const db = new Database(SQLITE_PATH_FILE, async (err) => {
     if (err) {
         console.log('Error when creating the database', err)
     } else {
         console.log('Database created!')
-        createTable();
+        await createTable();
+        await createTrigger();
+        startRegister();
+
     }
 });
 
 function createTable() {
-    db.run(`
-    
-    CREATE TABLE IF NOT EXISTS memory(
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        date TEXT,
-        total INT,
-        free INT,
-        used INT,
-        active INT,
-        available INT,
-        buffcache INT,
-        swaptotal INT,
-        swapused INT,
-        swapfree INT
-    )    
-    `
-        , (d, err) => {
-            // console.log(d);
-            // console.error(err);
-            db.run(`    
+    return new Promise((res, rej) => {
+        db.run(`
+        CREATE TABLE IF NOT EXISTS memory(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            date TEXT,
+            total INT,
+            free INT,
+            used INT,
+            active INT,
+            available INT,
+            buffcache INT,
+            swaptotal INT,
+            swapused INT,
+            swapfree INT
+        )    
+        `
+            , (d, err) => {
+                if (err) { rej(err); }
+                else { res(d); }
+            });
+    });
+
+}
+function createTrigger() {
+    return new Promise((res, rej) => {
+        db.run(`    
             CREATE TRIGGER DataSize AFTER INSERT ON memory
             BEGIN
-              delete from memory where 
-                id =(select min(id) from memory ) and (select count(*) from memory )=1001;
-            END`, (de, error) => {
-                    startRegister();
-                }
-            )
-        });
+            delete from memory where 
+                id =(select min(id) from memory ) and (select count(*) from memory )=${MAX_RECORDS};
+            END`,
+            (d, err) => {
+                if (err) { rej(err); }
+                else { res(d); }
+            }
+        )
+    });
 }
-
+function startRegister() {
+    setInterval(() => {
+        si.mem()
+            .then(data => insertData(data))
+            .catch(error => console.error(error));
+    }, 1000 * SECONDS_INTERVAL);
+}
 const insertData = (info) => {
-    console.log("Inserting data");
     db.run(`
     INSERT INTO memory (date, total, free, used, active, available, buffcache, swaptotal, swapused, swapfree) VALUES (?,?,?,?,?,?,?,?,?,?)`,
         [
@@ -85,31 +137,48 @@ const insertData = (info) => {
             info.swapfree,
         ]);
 }
-
-function startRegister() {
-    setInterval(() => {
-        si.mem()
-            .then(data => insertData(data))
-            .catch(error => console.error(error));
-    }, 1000 * 3);
+function jsonFormat(d) {
+    return {
+        date: d.date,
+        memory: {
+            total: d.total,
+            free: d.free,
+            used: d.used,
+            active: d.active,
+            available: d.available,
+            buffcache: d.buffcache,
+        },
+        swap: {
+            total: d.swaptotal,
+            used: d.swapused,
+            free: d.swapfree
+        }
+    }
 }
 
+const payload = { server: "supertanker" };
+
+app.get('/login', function (req, res) {
+    if (req.authType !== 'apikey') { res.send('loggin forbidden'); }
+    res.send(sign({ ...payload, from: 'Local server testing ..' }, privateKEY, signOptions));
+});
+app.get('/is-logged', function (req, res) {
+    res.json({ payload: req.jwtPayload, authType: req.authType, hola: "oliasflksajdfl" });
+});
 app.get('/memory', function (req, res) {
     si.mem()
-        .then(data => res.json(data))
+        .then(data => res.json(jsonFormat(data)))
         .catch(error => console.error(error));
 });
-
 app.get('/memory-history', function (req, res) {
 
     if (!isNaN(Number(req.query.limit))) { defaultMemoryRecords = Number(req.query.limit); }
 
-    db.all(`SELECT * FROM memory ORDER BY id DESC LIMIT ${defaultMemoryRecords}`, function (err, rows) {
-        if(err){ res.send(err); return;}
-        res.json(rows);
+    db.all(`SELECT * FROM memory ORDER BY id DESC LIMIT ${defaultMemoryRecords}`, (err, rows) => {
+        if (err) { res.send(err); return; }
+        res.json(rows.map(m => jsonFormat(m)));
     });
 });
-
-app.listen(3000, function () {
-    console.log('MemoryApp listening on port 3000!');
+app.listen(PORT, () => {
+    console.log(`MemoryApp listening on port ${PORT}!`);
 });
